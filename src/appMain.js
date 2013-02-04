bd43857c5478f2d923b1ccb4802a9ad8cd2aac37
saumya <saumya>
2013-02-04 17:42:41 +0530

    code cleanup

--- src/appMain.js
+++ src/appMain.js
@@ -1,40 +1,6 @@
-/*
-//console.log('Hello World : initialised from RequireJS');
 requirejs.config({
-    //To get timely, correct error triggers in IE, force a define/shim exports check.
+	//To get timely, correct error triggers in IE, force a define/shim exports check.
     enforceDefine: true,
-    // 3rd party script alias names (Easier to type "jquery" than "libs/jquery-1.8.3.min")
-    paths: {
-        // Core Libraries
-        "jquery": "../assets/js/jquery-1.9.0",
-        "underscore": "../assets/js/underscore-1.4.4",
-        "backbone": "../assets/js/backbone-0.9.10"
-    },
-    // Sets the configuration for your third party scripts that are not AMD compatible
-    shim: {
-    	"backbone": {
-    		"deps": [ "underscore", "jquery" ],
-    		"exports": "Backbone"  //attaches "Backbone" to the window object
-    	} 
-    }// end Shim Configuration
-});
-
-define("appMain", function(){
-	
-	require(['jquery','Backbone'],
-	function($,backbone){
-		// The "app" dependency is passed in as "App"
-		//App.initialize();
-		console.log('initialise');
-		console.log(backbone('#container'));
-		
-		}
-	);
-		
-});
-*/
-
-requirejs.config({
 	// 3rd party script alias names (Easier to type "jquery" than "libs/jquery-1.8.3.min")
     paths: {
         // Core Libraries
@@ -42,6 +8,7 @@ requirejs.config({
         "underscore": "../assets/js/underscore-1.4.4",
         "backbone": "../assets/js/backbone-0.9.10"
     },
+    // Sets the configuration for your third party scripts that are not AMD compatible
     shim: {
         'jquery': {
             exports: '$'
@@ -53,7 +20,7 @@ requirejs.config({
             deps: ['jquery'],
             exports: 'Backbone'
         }
-    }
+    }// end Shim Configuration
 });
 
 
@@ -61,12 +28,17 @@ define(
     ['jquery','underscore','backbone'],
 
     function (jQueryLocal,underscoreLocal,backboneLocal) {
+    	/*
         console.log('local', jQueryLocal);
         console.log('local', underscoreLocal);
         console.log('local', backboneLocal);
         console.log('global', $);
         console.log('global', _);
         console.log('global', Backbone);
+        */
+       console.log(jQueryLocal('#container'));
+       var a={'nameIs':'saumya'};
+       console.log(underscoreLocal('#container'))
     }
 );
 
